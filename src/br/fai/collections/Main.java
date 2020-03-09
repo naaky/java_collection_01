8fe0c3d54df7772f2aa196b36d166907ee81b6e9
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 20:20:13 -0300

    primeiro commit

---
+++ src/br/fai/collections/Main.java
@@ -0,0 +1,18 @@
+package br.fai.collections;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Main app = new Main();
+		app.start();
+
+	}
+
+	private void start() {
+		
+		System.out.println("uhsauhs");
+		
+	}
+
+}
